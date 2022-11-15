7998e1b1a114c057d9b4a1c19e59a7f5d823deb0
t-iffany <tiffanyxleong@gmail.com>
2022-11-15 01:27:06 +0000

    Created assertion function which compares two values and prints out a colourful successful or failure message

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    return console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+
