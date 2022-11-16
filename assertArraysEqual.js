3ad133ad921680b427be5152043c44d5b279a505
t-iffany <tiffanyxleong@gmail.com>
2022-11-16 04:07:08 +0000

    Created assertArraysEqual function which take two arrays and console.log a pass or fail message to the console.

---
+++ assertArraysEqual.js
@@ -0,0 +1,21 @@
+const eqArrays = function(a, b) {
+
+  if (a.length !== b.length) return console.log(`🛑🛑🛑 Assertion Failed: ${a} !== ${b}`);
+
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      return console.log(`🛑🛑🛑 Assertion Failed: ${a} !== ${b}`);
+    }
+  }
+  return console.log(`✅✅✅ Assertion Passed: ${a} === ${b}`);
+};
+
+
+
+/*test cases
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+*/
+
