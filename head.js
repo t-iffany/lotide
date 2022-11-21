f30b87ed904c7c7a900a724ce203e17df481e37a
t-iffany <tiffanyxleong@gmail.com>
2022-11-21 20:46:38 +0000

    Modularized code into separate files, refactored assertEqual.js and head.js, created test files.

--- head.js
+++ head.js
@@ -1,19 +1,10 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 
 const head = function(array) {
   return array[0]
 };
 
-//test assertions//
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
-
+module.exports = head;
 
---
+++ test/headTest.js
@@ -0,0 +1,12 @@
+// test/headTest.js
+
+const head = require('../head');
+
+// test/assertEqual.js
+
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
