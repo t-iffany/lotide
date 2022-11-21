ce88aa7938a39aeebb4fd8f04349ca1e1a8ae39f
t-iffany <tiffanyxleong@gmail.com>
2022-11-21 21:13:55 +0000

    Refactored tail.js and created tailTest.js

---
+++ test/tailTest.js
@@ -0,0 +1,40 @@
+// test/tailTest.js
+
+const tail = require('../tail');
+
+// test/assertEqual.js
+
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+
+// Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+
+/* Test Case 2: Check the returned array elements
+const result = tail(["Alpha", "Beta", "Charlie", "Delta"]);
+assertEqual(result.length, 3); // ensure we get back three elements
+assertEqual(result[0], "Beta"); // ensure first element is "Beta"
+assertEqual(result[1], "Charlie"); // ensure second element is "Charlie"
+assertEqual(result[2], "Delta"); // ensure third element is "Delta"
+*/
+
+/* Test Case 3: Array with only one element should yield an empty array for its tail
+const result = tail(["Hello"]);
+assertEqual(result.length, 0); // ensure we get back 0 elements
+const empty = '[]';
+tail(empty);
+assertEqual(empty, '[]'); // ensure empty array
+*/
+
+/* Test Case 4: Empty array should yield an empty array for its tail
+const result = tail([]);
+assertEqual(result.length, 0); // ensure we get back 0 elements
+const empty = '[]'
+tail(empty);
+assertEqual(empty, '[]'); // ensure empty array
+*/
